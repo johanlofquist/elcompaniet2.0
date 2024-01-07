6ebe9a8eb547ad10568b4118b38c0ef63de9f2a4
Johan Löfquist <johan.lofquist@gmail.com>
2024-01-07 15:58:08 +0100

    Added services

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,12 +1,22 @@
-document.addEventListener("DOMContentLoaded", function() {
-    var cursor = document.createElement("div");
-    cursor.classList.add("glow-cursor");
+document.addEventListener('DOMContentLoaded', function() {
+    var cursor = document.createElement('div');
+    cursor.classList.add('glow-cursor');
     document.body.appendChild(cursor);
 
-    document.addEventListener("mousemove", function(e) {
-        var x = e.pageX - 500;
-        var y = e.pageY - 500;
-        cursor.style.left = x + "px";
-        cursor.style.top = y + "px";
+    document.addEventListener('mousemove', function(e) {
+        var cursorSize = 1000; // Storleken på din glödeffekt
+        var halfSize = cursorSize / 2;
+
+        var x = e.clientX - halfSize;
+        var y = e.clientY - halfSize;
+
+        // Anpassa för skrollning
+        x += window.scrollX;
+        y += window.scrollY;
+
+        cursor.style.left = x + 'px';
+        cursor.style.top = y + 'px';
     });
 });
+
+
