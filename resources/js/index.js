7302253fa811ce5d834ac222b2e246af553e7d22
Johan Löfquist <johan.lofquist@gmail.com>
2024-03-09 10:03:25 +0100

    Text changes

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,55 +1,53 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const wrapper = document.createElement('div');
-    const cursor = document.createElement('div');
-    wrapper.classList.add('glow-cursor-wrapper');
-    cursor.classList.add('glow-cursor');
+document.addEventListener("DOMContentLoaded", function () {
+  const wrapper = document.createElement("div");
+  const cursor = document.createElement("div");
+  wrapper.classList.add("glow-cursor-wrapper");
+  cursor.classList.add("glow-cursor");
 
-    document.body.appendChild(wrapper);
-    wrapper.appendChild(cursor);
+  document.body.appendChild(wrapper);
+  wrapper.appendChild(cursor);
 
-    document.addEventListener('mousemove', function(e) {
-        var cursorSize = 1000; // Storleken på din glödeffekt
-        var halfSize = cursorSize / 2;
+  document.addEventListener("mousemove", function (e) {
+    var cursorSize = 1000; // Storleken på din glödeffekt
+    var halfSize = cursorSize / 2;
 
-        var x = e.clientX - halfSize;
-        var y = e.clientY - halfSize;
+    var x = e.clientX - halfSize;
+    var y = e.clientY - halfSize;
 
-
-
-        cursor.style.left = x + 'px';
-        cursor.style.top = y + 'px';
-    });
+    cursor.style.left = x + "px";
+    cursor.style.top = y + "px";
+  });
 });
 
-let hemBtn = document.getElementById("hem__btn")
-let tjansterBtn = document.getElementById("tjanster__btn")
-let omBtn = document.getElementById("om__btn")
-let kontaktBtn = document.getElementById("kontakt__btn")
+let hemBtn = document.getElementById("hem__btn");
+let tjansterBtn = document.getElementById("tjanster__btn");
+let omBtn = document.getElementById("om__btn");
+let kontaktBtn = document.getElementById("kontakt__btn");
 
-window.addEventListener("scroll", function(e) {
-    console.log(this.window.scrollY)
-    if (this.window.scrollY < 400) {
-        hemBtn.classList.add("active")
-        tjansterBtn.classList.remove("active")
-        omBtn.classList.remove("active")
-        kontaktBtn.classList.remove("active")
-    } else if (this.window.scrollY < 2000) {
-        hemBtn.classList.remove("active")
-        tjansterBtn.classList.add("active")
-        omBtn.classList.remove("active")
-        kontaktBtn.classList.remove("active")
-    } else if (this.window.scrollY < 2400) {
-        hemBtn.classList.remove("active")
-        tjansterBtn.classList.remove("active")
-        omBtn.classList.add("active")
-        kontaktBtn.classList.remove("active")
-    } else {
-        hemBtn.classList.remove("active")
-        tjansterBtn.classList.remove("active")
-        omBtn.classList.remove("active")
-        kontaktBtn.classList.add("active")
-    }
-})
+window.addEventListener("scroll", function (e) {
+  console.log(this.window.scrollY);
+  if (this.window.scrollY < 400) {
+    hemBtn.classList.add("active");
+    tjansterBtn.classList.remove("active");
+    omBtn.classList.remove("active");
+    kontaktBtn.classList.remove("active");
+  } else if (this.window.scrollY < 2000) {
+    hemBtn.classList.remove("active");
+    tjansterBtn.classList.add("active");
+    omBtn.classList.remove("active");
+    kontaktBtn.classList.remove("active");
+  } else if (this.window.scrollY < 2400) {
+    hemBtn.classList.remove("active");
+    tjansterBtn.classList.remove("active");
+    omBtn.classList.add("active");
+    kontaktBtn.classList.remove("active");
+  } else {
+    hemBtn.classList.remove("active");
+    tjansterBtn.classList.remove("active");
+    omBtn.classList.remove("active");
+    kontaktBtn.classList.add("active");
+  }
+});
 
 let burgerMenu = document.querySelector("#hamburger__menu");
 let lineOne = document.querySelector("#line__one");
@@ -83,9 +81,9 @@ burgerMenu.addEventListener("click", function () {
 });
 
 let menuHome = document.querySelector("#menu__hem");
-let menuServices = document.querySelector("#menu__services")
-let menuOm = document.querySelector("#menu__om")
-let menuKontakt = document.querySelector("#menu__kontakt")
+let menuServices = document.querySelector("#menu__services");
+let menuOm = document.querySelector("#menu__om");
+let menuKontakt = document.querySelector("#menu__kontakt");
 
 menuHome.addEventListener("click", function () {
   lineOne.classList.remove("burgerSpinOneToX");
